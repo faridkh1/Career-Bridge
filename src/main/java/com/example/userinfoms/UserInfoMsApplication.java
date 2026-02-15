5481bac11c6378fe0711d594c30f0770453fbc82
Farid Khalilov <009470@ibar.int>
2026-02-16 01:01:58 +0400

    First commit to user-info-ms

---
+++ src/main/java/com/example/userinfoms/UserInfoMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.userinfoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserInfoMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserInfoMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/userinfoms/UserInfoMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userinfoms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserInfoMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
